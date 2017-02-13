884fc590e149a1e5fe2006c84027dd4d4ce62c0b
Basel <BaselSafieh@gmail.com>
2017-02-13 15:42:33 +0100

    Sample code to connect the controller

---
+++ basicexample/controllers/controller1.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Basel on 2/13/2017.
+ */
+'use strict';
+
+angular.module('myApp.view1', ['ngRoute'])
+
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/view1', {
+            templateUrl: 'view1/view1.html',
+            controller: 'View1Ctrl'
+        });
+    }])
+
+    .controller('View1Ctrl', [function() {
+
+    }]);
