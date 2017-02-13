63a000c46bd55c6e5674ac15b7f7e0c0c956322d
Basel <BaselSafieh@gmail.com>
2017-02-13 15:39:40 +0100

    Sample code to connect the views with the rest of the code

--- basicexample/app.js
+++ basicexample/app.js
@@ -1,3 +1,17 @@
 /**
  * Created by Basel on 2/13/2017.
  */
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', [
+    'ngRoute',
+    'myApp.view1',
+    'myApp.view2',
+    'myApp.version'
+]).
+config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+    $locationProvider.hashPrefix('!');
+
+    $routeProvider.otherwise({redirectTo: '/view1'});
+}]);
